2b593c354b58638ce3e3f224a646adc1c31d7951
junt74 <junt74@gmail.com>
2017-01-21 13:39:20 +0900

    playerweapon.CurrentEnegry に応じた自機表示

--- UnityProject/Assets/Scripts/Battle/PlayerChargeUI.cs
+++ UnityProject/Assets/Scripts/Battle/PlayerChargeUI.cs
@@ -4,13 +4,19 @@ using UnityEngine;
 
 public class PlayerChargeUI : MonoBehaviour {
 
+	PlayerWeapon playerweapon;
+	RectTransform rt;
 	// Use this for initialization
 	void Start () {
-		
+		playerweapon = transform.parent.GetComponent<PlayerWeapon> ();
+		rt = GetComponent<RectTransform> ();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+		if (playerweapon) {
+			float p = playerweapon.CurrentEnegry / playerweapon.MaxEnegry;
+			rt.localScale = new Vector3 (p, p, 1);
+		}
 	}
 }
