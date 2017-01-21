a54e8a96b705d7241053ee15c5b22e51120e550a
junt74 <junt74@gmail.com>
2017-01-21 18:13:54 +0900

    とりあえずのエフェクトを追加

---
+++ UnityProject/Assets/FX/DestroyWhenParticleEnd.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyWhenParticleEnd : MonoBehaviour {
+
+	ParticleSystem ps;
+	// Use this for initialization
+	void Start () {
+		ps = GetComponent<ParticleSystem> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (ps) {
+			if (ps.IsAlive() == false) {
+				Destroy (gameObject);
+			}
+		}
+	}
+}
