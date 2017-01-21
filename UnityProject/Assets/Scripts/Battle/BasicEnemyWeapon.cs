57b65061dbf03ee1f7da745ce78ab6f6835dda24
fumi <fumilin01@gmail.com>
2017-01-21 16:30:15 +0900

    敵の武器を追加

---
+++ UnityProject/Assets/Scripts/Battle/BasicEnemyWeapon.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BasicEnemyWeapon : MonoBehaviour , IWeapon
+{
+	public GameObject bulletPrefab;
+
+	public void Shot()
+	{
+		var parent = this.transform.parent;
+		var prefab = GameObject.Instantiate<GameObject>(bulletPrefab, parent);
+		prefab.transform.localPosition = this.transform.localPosition;
+
+		var bullet = prefab.GetComponent<IBullet>();
+		var direction = new Vector2(-1, 0);
+		bullet.Init(direction, 1);
+	}
+}
+
