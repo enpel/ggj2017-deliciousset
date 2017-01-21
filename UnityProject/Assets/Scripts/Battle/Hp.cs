bc1078f94de4337912fdb25c25d7177c54868997
fumi <fumilin01@gmail.com>
2017-01-21 16:01:42 +0900

    hpを追加

---
+++ UnityProject/Assets/Scripts/Battle/Hp.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UniRx;
+using UniRx.Triggers;
+
+public class Hp : MonoBehaviour
+{
+	public float MaxHP;
+
+	public ReactiveProperty<bool> IsDead = new ReactiveProperty<bool>();
+	void UpdateDeadState()
+	{
+		IsDead.Value = (CurrentHP.Value == 0);
+	}
+	public ReactiveProperty<float> CurrentHP = new ReactiveProperty<float>();
+	void UpdateHP(float newHP)
+	{
+		CurrentHP.Value = Mathf.Clamp(newHP, 0, MaxHP);
+		UpdateDeadState();
+	}
+
+	void Start()
+	{
+		Init();
+	}
+
+	public void Init()
+	{
+		CurrentHP.Value = MaxHP;
+	}
+
+	public void OnDamage(float damage)
+	{
+		UpdateHP(CurrentHP.Value - damage);
+	}
+
+	public void OnRecovery(float recovery)
+	{
+		UpdateHP(CurrentHP.Value + recovery);
+	}
+}
