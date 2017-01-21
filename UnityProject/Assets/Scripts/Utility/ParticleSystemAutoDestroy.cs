a319a884722dc6f3b3d10b0b3bf18a1fd4d5520e
fumi <fumilin01@gmail.com>
2017-01-21 18:07:31 +0900

    パーティクルシステムを自動的に削除するスクリプトを追加

---
+++ UnityProject/Assets/Scripts/Utility/ParticleSystemAutoDestroy.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+// http://answers.unity3d.com/questions/219609/auto-destroying-particle-system.html
+public class ParticleSystemAutoDestroy : MonoBehaviour 
+{
+	private ParticleSystem ps;
+
+
+	public void Start() 
+	{
+		ps = GetComponent<ParticleSystem>();
+	}
+
+	public void Update() 
+	{
+		if(ps)
+		{
+			if(!ps.IsAlive())
+			{
+				Destroy(gameObject);
+			}
+		}
+	}
+}
