fc5b38ef3d484a5d83db27adff3ee6fe76d5a75f
fumi <fumilin01@gmail.com>
2017-01-21 18:37:24 +0900

    プレイヤーは敵の攻撃を受けて、HPが0になるとログを表示する

--- UnityProject/Assets/Scripts/Battle/Player.cs
+++ UnityProject/Assets/Scripts/Battle/Player.cs
@@ -12,6 +12,12 @@ public class Player : MonoBehaviour
 	{
 		weapon = GetComponent<PlayerWeapon>();
 		hp = GetComponent<Hp>();
+
+		hp.IsDead.Where(dead => dead)
+				.Subscribe(dead =>
+				{
+					Debug.Log("Playerがやられたので、多分ゲームオーバーだと思う。");
+				});
 	}
 
 	void Start()
@@ -22,6 +28,9 @@ public class Player : MonoBehaviour
 
 	void OnTriggerEnter2D(Collider2D other)
 	{
-		Debug.Log(other.name + "にあたった" );
+		var bullet = other.GetComponent<BulletBase>();
+		var damage = bullet.CurrentPower;
+		hp.OnDamage(damage);
+		Debug.Log(string.Format("{0}は{1}に{2}ポイントのダメージ{3}", other.name, gameObject.name, damage, hp.ToDisplayString()));
 	}
 }
