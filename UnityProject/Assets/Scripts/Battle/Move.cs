711aa418b5f618b8cdbfc1c5fab2ef40f1f6c832
fumi <fumilin01@gmail.com>
2017-01-21 16:28:37 +0900

    敵の移動スクリプトをとりあえず追加

---
+++ UnityProject/Assets/Scripts/Battle/Move.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Move : MonoBehaviour
+{
+	public Transform target;
+	public float Speed;
+
+	void Update()
+	{
+		var direction = new Vector2(-1, 0);
+		transform.Translate(direction * Speed * Time.deltaTime);
+	}
+}
