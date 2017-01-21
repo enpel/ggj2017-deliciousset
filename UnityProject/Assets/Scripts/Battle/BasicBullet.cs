ce1917eb153330109f780fb1c7b425f538668709
fumi <fumilin01@gmail.com>
2017-01-21 15:21:40 +0900

    弾を追加

---
+++ UnityProject/Assets/Scripts/Battle/BasicBullet.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BasicBullet : MonoBehaviour, IBullet
+{
+	[SerializeField] float requireEnergy;
+	public float RequireEnergy { get { return requireEnergy; } }
+	public float BasePower;
+	public float Speed;
+
+	public float CurrentPower
+	{
+		get { return BasePower * multipliePower; }
+	}
+
+	private float multipliePower;
+	private Transform trans;
+	private Transform target;
+	private Vector2 direction;
+
+	private Rigidbody2D bulletRigidbody;
+	
+	public void Init(Transform target, float multipliePower)
+	{
+		this.target = target;
+		this.multipliePower = multipliePower;
+	}
+
+	public void Init(Vector2 direction, float multipliePower)
+	{
+		this.multipliePower = multipliePower;
+		this.direction = direction;
+	}
+
+	void SetSize()
+	{
+		trans.localScale = Vector3.one * Mathf.Sqrt(CurrentPower);
+	}
+
+	void Start()
+	{
+		trans = transform;
+		SetSize();
+		bulletRigidbody = GetComponent<Rigidbody2D>();
+		bulletRigidbody.AddForce(direction * Speed);
+	}
+
+	void OnTriggerEnter(Collider other)
+	{
+		var damage = CurrentPower * multipliePower;
+		Debug.Log(string.Format("{0}にあたった", other.name, damage) );
+		Destroy(this.gameObject);
+	}
+}
---
+++ UnityProject/Assets/Scripts/Battle/IBullet.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public interface IBullet
+{
+	float RequireEnergy {get;}
+	void Init(Transform target, float multipliePower);
+	void Init(Vector2 direction, float multipliePower);
+}
