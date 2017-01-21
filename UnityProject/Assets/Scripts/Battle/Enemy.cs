6602d344edecf74a065076e44a35a71ee8d85bc9
fumi <fumilin01@gmail.com>
2017-01-21 16:29:04 +0900

    敵のスクリプトを追加

---
+++ UnityProject/Assets/Scripts/Battle/Enemy.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+	public IWeapon weapon { get; private set; }
+	public Move move{get; private set;}
+	public Hp hp {get; private set;}
+
+	void Awake()
+	{
+		weapon = GetComponent<IWeapon>();
+		move = GetComponent<Move>();
+		hp = GetComponent<Hp>();
+	}
+
+	void OnTriggerEnter(Collider other)
+	{
+		Debug.Log(other.name + "にあたった" );
+	}
+}
