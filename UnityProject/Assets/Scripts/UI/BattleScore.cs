8d390191e2b8e3db00929b5592ccad94c12ac7ef
junt74 <junt74@gmail.com>
2017-01-21 16:31:10 +0900

    バトルシーンに体力ゲージを追加

--- UnityProject/Assets/Scripts/UI/BattleScore.cs
+++ UnityProject/Assets/Scripts/UI/BattleScore.cs
@@ -1,16 +1,21 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UniRx;
+using UniRx.Triggers;
 
 public class BattleScore : MonoBehaviour {
 
+	Player player;
+	Text text;
+
 	// Use this for initialization
 	void Start () {
-		
+		player = transform.parent.GetComponent<Player> ();
 	}
-	
+
 	// Update is called once per frame
 	void Update () {
-		
 	}
 }
