9d4d64ba972404d8725c1e310ca06fe7ff3947a7
junt74 <junt74@gmail.com>
2017-01-21 15:08:12 +0900

    ボタン素材（スタート／リトライ／やめる） アイテムパネルをプレハブ化、要素を変更

---
+++ UnityProject/Assets/Scripts/UI/Item.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Item : MonoBehaviour {
+
+	public Text havecountText;
+	public Image itemIcon;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
