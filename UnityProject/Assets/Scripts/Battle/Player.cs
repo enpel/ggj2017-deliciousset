cd32e74c7fa03510b51dd698e3810676e82d64a2
fumi <fumilin01@gmail.com>
2017-01-21 12:32:27 +0900

    プレイヤーは武器を使って攻撃できる

---
+++ UnityProject/Assets/Scripts/Battle/Player.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UniRx;
+
+public class Player : MonoBehaviour
+{
+	IWeapon weapon;
+
+	void Awake()
+	{
+		weapon = GetComponent<IWeapon>();
+	}
+
+	void Start()
+	{
+		MyInput.GetInputStream()
+			.Subscribe(x => weapon.Shot());
+	}
+}
---
+++ UnityProject/Assets/Scripts/Battle/PlayerWeapon.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerWeapon : MonoBehaviour , IWeapon
+{
+	public float MaxEnegry = 5;
+	public float BaseEnegryChargeRate = 2;
+
+	float _currentEnergy = 0;
+	public float CurrentEnegry
+	{
+		get
+		{
+			return _currentEnergy;
+		}
+		private set
+		{
+			_currentEnergy = Mathf.Min(MaxEnegry, value);
+		}
+	}
+
+	void Start ()
+	{
+		CurrentEnegry = 0;
+	}
+
+	void Update()
+	{
+		CurrentEnegry += BaseEnegryChargeRate * Time.deltaTime;
+	}
+
+	public void Shot()
+	{
+		Debug.Log("shot" + CurrentEnegry);
+		CurrentEnegry = 0;
+	}
+}
