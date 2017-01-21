948e13cb7162030bba96e4d48c2d1f1214b932b7
fumi <fumilin01@gmail.com>
2017-01-21 12:01:01 +0900

    入力関数(スペースやタップで入力を返す）を追加 使い方はこんな感じ ``` using UniRx;
    		MyInput.GetInputStream().Subscribe(x => Debug.Log("aaa"));

---
+++ UnityProject/Assets/Scripts/Core/MyInput.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UniRx;
+using UniRx.Triggers;
+
+public class MyInput
+{
+	static IObservable<Unit> onInput;
+
+	public static IObservable<Unit> GetInputStream()
+	{
+		if (onInput == null)
+		{
+			onInput = CreateInputStream();
+		}
+
+		return onInput;
+	}
+
+	static IObservable<Unit> CreateInputStream()
+	{
+		var updateHolder = new GameObject();
+		var stream = updateHolder.UpdateAsObservable()
+						.Where(_ => 
+						{
+							if (Application.platform == RuntimePlatform.Android
+							&& Application.platform == RuntimePlatform.IPhonePlayer)
+							{
+								return Input.GetMouseButtonDown(0);
+							}
+							else
+							{
+								return Input.GetKeyDown(KeyCode.Space);
+							}
+						})
+						.Publish()
+						.RefCount();
+		return stream;
+	}
+}
