24aaf41f01b70ac34ee2d29a24a17134ec27b781
fumi <fumilin01@gmail.com>
2017-01-22 16:17:30 +0900

    waveの進行度に応じてステージ(背景)が切り替わる

---
+++ UnityProject/Assets/Scripts/Battle/Stage.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class Stage : MonoBehaviour
+{
+	List<ParticleSystem> particles;
+
+	void Start()
+	{
+		particles = GetComponentsInChildren<ParticleSystem>().ToList();
+	}
+
+	public void Show()
+	{
+		foreach(var particle in particles)
+		{
+			var emit = particle.emission;
+			emit.enabled = true;
+		}
+	}
+
+	public void Hide()
+	{
+		foreach(var particle in particles)
+		{
+			var emit = particle.emission;
+			emit.enabled = false;
+		}
+	}
+}
--- UnityProject/Assets/Scripts/Manager/StageManager.cs
+++ UnityProject/Assets/Scripts/Manager/StageManager.cs
@@ -1,13 +1,32 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UniRx;
 
 public class StageManager : SingletonMonoBehaviour<StageManager>
 {
-	[SerializeField] List<GameObject> stages;
+	[SerializeField] List<Stage> stages;
+
+	int currentStageNum;
 
 	void Start()
 	{
-		
+		foreach(var stage in stages)
+		{
+			stage.Hide();
+		}
+
+		GameManager.Instance.Wave.Subscribe(waveNum =>{
+			var newStageNum = (waveNum - 1) % stages.Count;
+			ChangeStage(newStageNum);
+		});
+	}
+
+	void ChangeStage(int newStageNum)
+	{
+		stages[currentStageNum].Hide();
+
+		stages[newStageNum].Show();
+		currentStageNum = newStageNum;
 	}
 }
