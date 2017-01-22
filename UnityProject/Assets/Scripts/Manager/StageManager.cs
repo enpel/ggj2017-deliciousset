4c2546d132c1a6c895e7c9c7d551e89ef075b810
fumi <fumilin01@gmail.com>
2017-01-22 15:54:39 +0900

    バトルシーンに背景追加

---
+++ UnityProject/Assets/Scripts/Manager/StageManager.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StageManager : SingletonMonoBehaviour<StageManager>
+{
+	[SerializeField] List<GameObject> stages;
+
+	void Start()
+	{
+		
+	}
+}
